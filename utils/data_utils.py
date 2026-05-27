aa3bab988b3be368744b3b9dddde39c99c1c56b3
Hicaro Calliari Bottenberg <hicaro.bottenberg@gmail.com>
2026-05-27 16:41:19 -0300

    add padronizar_genero and padronizar_estado_civil

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -11,3 +11,30 @@ def padroniza_genero(df):
     
     """
     return df['CL_GENERO'].str.upper().map({'F':'FEMININO','M':'MASCULINO'})
+
+def padroniza_estado_civil(df):
+    """
+    Converte os códigos numéricos da coluna CL_EC, usando um dicionário catalogando cada valor correspondente em str
+    
+    1 -> Casado ou união estável
+    2 -> Divorciado
+    3 -> Separado
+    4 -> Solteiro
+    5 -> Viúvo
+
+    Parâmetros:
+        df: DataFrame contendo a coluna CL_EC
+    Retorna:
+        df['CL_EC'] atualizado
+    """
+
+    dicionario = ({ 
+
+        1: 'CASADO OU UNIÃO ESTÁVEL',
+        2: 'DIVORCIADO',
+        3: 'SEPARADO',
+        4: 'SOLTEIRO',
+        5: 'VIÚVO'
+
+    })
+    return df['CL_EC'].map(dicionario)
